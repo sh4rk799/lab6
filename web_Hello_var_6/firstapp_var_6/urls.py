eb5f59ab4088e5f618a71cee38b4e7078f612046
stepa <stepakud@gmail.com>
2025-10-09 13:04:46 +0300

    gtybc

--- web_Hello_var_6/firstapp_var_6/urls.py
+++ web_Hello_var_6/firstapp_var_6/urls.py
@@ -8,4 +8,15 @@ urlpatterns = [
     path('warehouse/', views.warehouse, name='warehouse'),  # ← Добавьте эту строку
     path('shops/', views.shops, name='shops'),
     path('storekeeper/', views.storekeeper, name='storekeeper'),
-    path('table/', views.table, name='table'),]
+    path('table/', views.table, name='table'),
+    # Товары
+    path('warehouse/product1/', views.product1),
+    path('warehouse/product2/', views.product2),
+    path('warehouse/product3/', views.product3),
+
+    # Магазины
+    path('shops/magnit/', views.magnit),
+    path('shops/dns/', views.dns),
+    path('shops/pyaterochka/', views.pyaterochka),
+    path('shops/mvideo/', views.mvideo),
+    ]
--- web_Hello_var_6/firstapp_var_6/views.py
+++ web_Hello_var_6/firstapp_var_6/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def index(request):
     return render(request, 'index.html')
@@ -14,3 +15,25 @@ def storekeeper(request):
 
 def table(request):
     return render(request, 'table.html')
+
+def product1(request):
+    return render(request, 'products/product1.html')
+
+def product2(request):
+    return render(request, 'products/product2.html')
+
+def product3(request):
+    return render(request, 'products/product3.html')
+
+# Функции для магазинов
+def magnit(request):
+    return render(request, 'shops/magnit.html')
+
+def dns(request):
+    return render(request, 'shops/dns.html')
+
+def pyaterochka(request):
+    return render(request, 'shops/pyaterochka.html')
+
+def mvideo(request):
+    return render(request, 'shops/mvideo.html')
